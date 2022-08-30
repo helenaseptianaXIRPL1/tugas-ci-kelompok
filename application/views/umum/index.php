b8ef6473cab956411ae9b3ca29599dd05dc65a62
helenaseptianaXIRPL1 <helenagaming1@gmail.com>
2022-08-30 21:40:43 +0700

    first commit

---
+++ application/views/umum/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website</title>
+</head>
+<body>
+    <center><h1>Kelompok</h1></center>
+    <form align="center">
+        <input type="text" placeholder="Cari...">
+        <input type="button" value="Klik">
+    </form>
+    <center><h2>Anggota</h2></center>
+    <center><h3>Helena Septiana</h3></center>
+    <center><h3>Indah Nurhamidah</h3></center>
+    <center><h3>Paramitha Cantika</h3></center>
+    <center><h3>M.Herdiansyah</h3></center>
+
+</body>
+</html>
